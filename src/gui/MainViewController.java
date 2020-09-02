53974fbe50213efb492b63b65cb3548aaaf7f296
Gregory Moser <gdc.moser@hotmail.com>
2020-09-02 22:54:21 +0100

    Tela Sobre

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 	
@@ -17,6 +26,9 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	private MenuItem menuItemViatura;
+	
+	@FXML
+	private MenuItem menuItemSobre;
 	
 	@FXML
 	private MenuItem menuItemManual;
@@ -37,6 +49,11 @@ public class MainViewController implements Initializable{
 	}
 	
 	@FXML
+	public void onMenuItemSobreAction() {
+		loadView("/gui/Sobre.fxml");
+	}
+	
+	@FXML
 	public void onMenuItemManualAction() {
 		System.out.println("onMenuItemManualAction");
 	}
@@ -44,5 +61,23 @@ public class MainViewController implements Initializable{
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		
+	}
+	
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader (getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		}catch(IOException e) {
+			Alerts.showAlert("IOException", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
