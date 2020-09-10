a4ac26c8feb3e1c7c7baad88545d1c4243ae2844
Gregory Moser <gdc.moser@hotmail.com>
2020-09-10 17:18:27 +0100

    Atualizar Subunidade

--- src/gui/SubunidadeListController.java
+++ src/gui/SubunidadeListController.java
@@ -9,6 +9,7 @@ import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -27,39 +29,43 @@ import javafx.stage.Stage;
 import model.entities.Subunidade;
 import model.services.SubunidadeService;
 
-public class SubunidadeListController implements Initializable, DataChangeListener{
-	
+public class SubunidadeListController implements Initializable, DataChangeListener {
+
 	private SubunidadeService service;
-	
+
 	@FXML
 	private TableView<Subunidade> tableViewSubunidade;
-	
+
 	@FXML
 	private TableColumn<Subunidade, Integer> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Subunidade, String> tableColumnNome;
-	
+
+	@FXML
+	private TableColumn<Subunidade, Subunidade> tableColumnEDIT;
+
 	@FXML
 	private Button btNovo;
-	
+
 	private ObservableList<Subunidade> obsList;
-	
+
 	@FXML
 	public void onBtNovoAction(ActionEvent event) {
-		//Captura referencia para o Stage atual
+		// Captura referencia para o Stage atual
 		Stage parentStage = Utils.currentStage(event);
 		Subunidade obj = new Subunidade();
-		//Define a ação de clicar no botão como chamada do método que habilita janela do formulário
+		// Define a ação de clicar no botão como chamada do método que habilita janela
+		// do formulário
 		createDialogForm(obj, "/gui/SubunidadeForm.fxml", parentStage);
 	}
-	
+
 	// injeção de dependência manual, sem framework ou cotainer
-	//de injeção de dependência automática.
+	// de injeção de dependência automática.
 	public void setSubunidadeService(SubunidadeService service) {
 		this.service = service;
 	}
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		initializeNodes();
@@ -68,58 +74,81 @@ public class SubunidadeListController implements Initializable, DataChangeListen
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
-		
-		Stage stage = (Stage)Main.getMainScene().getWindow();
+
+		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewSubunidade.prefHeightProperty().bind(stage.heightProperty());
 	}
-	
+
 	public void updateTableView() {
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("Service estava nulo!");
 		}
 		List<Subunidade> list = service.findAll();
-		//instanciar observable list utilizando dados originais da lista
+		// instanciar observable list utilizando dados originais da lista
 		obsList = FXCollections.observableArrayList(list);
-		tableViewSubunidade.setItems(obsList); 
+		tableViewSubunidade.setItems(obsList);
+		initEditButtons();
 	}
-	
+
 	private void createDialogForm(Subunidade obj, String absoluteName, Stage parentStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader (getClass().getResource(absoluteName));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
-			
+
 			SubunidadeFormController controller = loader.getController();
-			//injetando subunidade no controlador
+			// injetando subunidade no controlador
 			controller.setSubunidade(obj);
-			////injetando subunidadeService no controlador
+			//// injetando subunidadeService no controlador
 			controller.setSubunidadeService(new SubunidadeService());
-			//Autoinscrição para receber o evento
+			// Autoinscrição para receber o evento
 			controller.subscribeDataChangeListener(this);
-			//carregar os dados do objeto no formulário
+			// carregar os dados do objeto no formulário
 			controller.updateFormData();
-			
-			//Instancia novo Stage
+
+			// Instancia novo Stage
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Nova Subunidade");
 			dialogStage.setScene(new Scene(pane));
-			//Proibir redimensionar a janela
+			// Proibir redimensionar a janela
 			dialogStage.setResizable(false);
-			//Definindo Stage pai desta janela
+			// Definindo Stage pai desta janela
 			dialogStage.initOwner(parentStage);
-			//Informa se a janela é modal ou se tem outro comportamento
-			//Window Modal mantém a janela travada até ser fechada, para novamente acessar a janela anterior
+			// Informa se a janela é modal ou se tem outro comportamento
+			// Window Modal mantém a janela travada até ser fechada, para novamente acessar
+			// a janela anterior
 			dialogStage.initModality(Modality.WINDOW_MODAL);
-			//Mostrar e aguardar
+			// Mostrar e aguardar
 			dialogStage.showAndWait();
-		}
-		catch(IOException e) {
+		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
 	@Override
 	public void onDataChanged() {
-		//Atualiza os dados da tabela
+		// Atualiza os dados da tabela
 		updateTableView();
+	}
+
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Subunidade, Subunidade>() {
+			private final Button button = new Button("editar");
+			
+			@Override
+			protected void updateItem(Subunidade obj, boolean empty) {
+				super.updateItem(obj, empty);
+				
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				} 
+				
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(
+								obj, "/gui/SubunidadeForm.fxml",Utils.currentStage(event))); 
+			}
+		});
 	}
 }
